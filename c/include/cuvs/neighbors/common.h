5ba81f35144db77110e825fe42ac3a1294232564
James Xia <jamxia@nvidia.com>
2026-07-03 14:36:52 -0700

    Remove unused #include

--- c/include/cuvs/neighbors/common.h
+++ c/include/cuvs/neighbors/common.h
@@ -1,11 +1,10 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2024-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
+ * SPDX-FileCopyrightText: Copyright (c) 2024-2026, NVIDIA CORPORATION.
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #pragma once
 
-#include <dlpack/dlpack.h>
 #include <stdint.h>
 
 #include <cuvs/core/export.h>
