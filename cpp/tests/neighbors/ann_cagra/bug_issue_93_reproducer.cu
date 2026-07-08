11f78eff8415c3a4d21ff62f25d06fc5b7c1a9db
HowardHuang1 <howardhuangb@gmail.com>
2026-07-08 14:23:24 -0700

    run pre-commit for new copyright style

--- cpp/tests/neighbors/ann_cagra/bug_issue_93_reproducer.cu
+++ cpp/tests/neighbors/ann_cagra/bug_issue_93_reproducer.cu
@@ -1,5 +1,5 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  *
  * Reproducer for https://github.com/rapidsai/cuvs-lucene/issues/93
