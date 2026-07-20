80a239edc3e43507c79701d10265937856711e91
tarangj <jaintarang2015@gmail.com>
2026-07-20 10:45:45 -0700

    update docs

--- c/include/cuvs/cluster/kmeans.h
+++ c/include/cuvs/cluster/kmeans.h
@@ -267,7 +267,7 @@ typedef enum { CUVS_KMEANS_TYPE_KMEANS = 0, CUVS_KMEANS_TYPE_KMEANS_BALANCED = 1
  *   k-means++ algorithm.
  *
  *   X may reside on either host (CPU) or device (GPU) memory.
- *   When X is on the host the data is streamed to the GPU in
+ *   When X is on the host the data is buffered to the GPU in
  *   batches controlled by params->device_buffer_samples.
  *
  * @note In cuVS 26.08 (next ABI major version) this signature will be
