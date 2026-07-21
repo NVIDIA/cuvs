425854c5a8777d684b44f20eec505539b7c57a2e
Julian Miller <julianmiller@nvidia.com>
2026-07-21 11:49:41 +0200

    Make ACE disk workspaces transactional and non-destructive

--- cpp/tests/neighbors/ann_hnsw_ace/test_half_uint32_t.cu
+++ cpp/tests/neighbors/ann_hnsw_ace/test_half_uint32_t.cu
@@ -1,11 +1,21 @@
 /*
- * SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION.
+ * SPDX-FileCopyrightText: Copyright (c) 2025-2026, NVIDIA CORPORATION & AFFILIATES. All rights reserved.
  * SPDX-License-Identifier: Apache-2.0
  */
 
 #include "../ann_hnsw_ace.cuh"
 
 namespace cuvs::neighbors::hnsw {
+
+TEST(CagraAceWorkspaceHalf, FailurePreservesCallerDirectory)
+{
+  test_ace_workspace_failure_preserves_caller_directory<half>();
+}
+
+TEST(CagraAceWorkspaceHalf, FailureDoesNotTruncateExistingArtifact)
+{
+  test_ace_workspace_failure_does_not_truncate_existing_artifact<half>();
+}
 
 typedef AnnHnswAceTest<float, half, uint32_t> AnnHnswAceTest_half;
 TEST_P(AnnHnswAceTest_half, AnnHnswAceBuild) { this->testHnswAceBuild(); }
