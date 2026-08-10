6ad9234655a846385ea8b839e5f569335fe95f6d
aamijar <aamijar230@gmail.com>
2026-08-10 22:12:59 +0000

    revert test to minimize diff

--- cpp/tests/neighbors/ann_cagra.cuh
+++ cpp/tests/neighbors/ann_cagra.cuh
@@ -1627,12 +1627,12 @@ inline std::vector<AnnCagraInputs> generate_inputs()
   inputs2 = raft::util::itertools::product<AnnCagraInputs>(
     {100},
     {1000},
-    {1, 3, 5, 7, 8, 17, 64, 128, 137, 192, 256, 512, 768, 1024},  // dim
-    {16},   // k
-    {32},   // degree
+    {1, 3, 5, 7, 8, 17, 64, 128, 137, 192, 256, 512, 1024},  // dim
+    {16},                                                    // k
+    {32},                                                    // degree
     {graph_build_algo::IVF_PQ,
      graph_build_algo::NN_DESCENT,
-     graph_build_algo::ITERATIVE_CAGRA_SEARCH}, // Iterative cagra q build
+     graph_build_algo::ITERATIVE_CAGRA_SEARCH},
     {search_algo::AUTO},
     {10},
     {0},
