aa5a8ebac1f3dcacbe263db4a4bd394fc343d2b0
Artem M. Chirkin <9253178+achirkin@users.noreply.github.com>
2026-08-21 10:53:29 +0200

    Update use_wheels_from_prs.sh

--- ci/use_wheels_from_prs.sh
+++ ci/use_wheels_from_prs.sh
@@ -8,8 +8,8 @@ source rapids-init-pip
 RAPIDS_PY_CUDA_SUFFIX=$(rapids-wheel-ctk-name-gen "${RAPIDS_CUDA_VERSION}")
 
 # download wheels, store the directories holding them in variables
-LIBRAFT_WHEELHOUSE=$(rapids-get-pr-artifact raft 3052 cpp wheel)
-PYLIBRAFT_WHEELHOUSE=$(rapids-get-pr-artifact raft 3052 python wheel --pkg_name pylibraft)
+LIBRAFT_WHEELHOUSE=$(rapids-get-pr-artifact raft 3121 cpp wheel)
+PYLIBRAFT_WHEELHOUSE=$(rapids-get-pr-artifact raft 3121 python wheel --pkg_name pylibraft)
 
 # write a pip constraints file saying e.g. "whenever you encounter a requirement for 'librmm-cu12', use this wheel"
 cat > "${PIP_CONSTRAINT}" <<EOF
